64515f87b2a4a7acb2a0a3b9960b7ce5b246490f
andrewksloan <andrewksloan3@gmail.com>
2021-01-17 19:08:26 -0800

    Fixed .git

---
+++ forgetful-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Objects from './Objects/Objects'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Forgetful Project
+        </p>
+        <Objects />
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ forgetful-frontend/src/Objects/Objects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Objects.css'
+
+const Objects = (prop) => {
+    return (
+        <div>
+            <p> This is a basic object. </p>
+        </div>
+    )
+}
+
+export default Objects
